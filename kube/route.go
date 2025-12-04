79849df97b1b6ea7554a791b99ca11f8ab2b5d8b
pschulten <peter.schulten@dbschenker.com>
2025-12-04 11:33:27 +0100

    don't fail if gateway api is not installed (#12)

--- kube/route.go
+++ kube/route.go
@@ -2,8 +2,11 @@ package kube
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 
+	errv1 "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	gwclient "sigs.k8s.io/gateway-api/pkg/client/clientset/versioned"
@@ -30,6 +33,11 @@ func getHTTPRoutes(c gwclient.Interface) []string {
 
 	httpRouteList, err := c.GatewayV1().HTTPRoutes(metav1.NamespaceAll).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
+		var apiErr *errv1.StatusError
+		if errors.As(err, &apiErr) && apiErr.Status().Code == http.StatusNotFound {
+			// no routes, no urls
+			return ret
+		}
 		panic(err.Error())
 	}
 
